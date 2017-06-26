e992737048909cbf7fd2a09510d8703d261286f2
Le Roy Ronan <ronan@leroy.com>
2017-06-27 00:27:11 +0200

    Refactor in order to go TDD

---
+++ modules/src/test/cs/CivilianPopulationCoreTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using CivilianPopulation;
+
+namespace CivilianPopulationShould
+{
+	[TestFixture()]
+	public class CivilianPopulationCoreTests
+    {
+		[Test()]
+		public void taxes_should_happen_every_6_hours()
+        {
+            double sixHours = 6 * 60 * 60;
+
+            TestAdapter adapter = new TestAdapter();
+            CivilianPopulationConfiguration configuration = new CivilianPopulationConfiguration();
+            configuration.setRentAmountPerCivilian(200);
+            configuration.setTimeBetweenRents(sixHours);
+
+            adapter.setUniversalTime(0);
+            CivilianPopulationCore core = new CivilianPopulationCore(adapter, configuration);
+
+            adapter.setUniversalTime(1);
+            double next = core.getTimeUntilTaxes();
+            Assert.IsTrue(next == sixHours - 1);
+        }
+    }
+}
---
+++ modules/src/test/cs/TestAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using CivilianPopulation;
+
+namespace CivilianPopulationShould
+{
+	internal class TestAdapter : CivilianPopulationAdapter
+	{
+		public double universalTime;
+
+		public void addFunds(int amount)
+		{
+			System.Console.Write("add " + amount + " founds");
+		}
+
+		public double getUniversalTime()
+		{
+			return this.universalTime;
+		}
+		public void setUniversalTime(double universalTime)
+		{
+			this.universalTime = universalTime;
+		}
+
+		public List<CivilianVessel> getVessels()
+		{
+			return new List<CivilianVessel>();
+		}
+
+		public bool isCareer()
+		{
+			return true;
+		}
+
+	}
+}

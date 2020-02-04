32df85b47212c0b849aad319545b4cb0668eb08f
Jonathan Bayer <linuxgurugamer@gmail.com>
2020-02-04 00:31:19 -0500

    Adoption by Linuxgurugamer  Added bulkheadProfiles to all parts  Fixed bad chars in some parts preventing them from loading  Reorganized directories  Created overall solution file  Added Assemblyversion.tt to all dlls  Added InstallChecker to all dlls  Adjusted node positions on many parts  Replaced stock toolbar code with ToolbarController  Added support for the ClickThroughBlocker  Added unique numbers for the window ids

--- install-sandbox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf /Users/rleroy/Games/KSP/1.4.3/KSP-CivPop-Sandbox/ksp-civilian-population-mod-*
-rm -rf /Users/rleroy/Games/KSP/1.4.3/KSP-CivPop-Sandbox/GameData/CivilianPopulation
-cp /Users/rleroy/git/CivilianPopulation/mod/target/ksp-civilian-population-mod-*.zip /Users/rleroy/Games/KSP/1.4.3/KSP-CivPop-Sandbox/GameData/.
-cd /Users/rleroy/Games/KSP/1.4.3/KSP-CivPop-Sandbox/GameData
-unzip ksp-civilian-population-mod-*.zip
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf /Users/rleroy/Games/KSP/1.4.5/KSP-TACLS/ksp-civilian-population-mod-*
-rm -rf /Users/rleroy/Games/KSP/1.4.5/KSP-TACLS/GameData/CivilianPopulation
-cp /Users/rleroy/git/CivilianPopulation/mod/target/ksp-civilian-population-mod-*.zip /Users/rleroy/Games/KSP/1.4.5/KSP-TACLS/GameData/.
-cd /Users/rleroy/Games/KSP/1.4.5/KSP-TACLS/GameData
-unzip ksp-civilian-population-mod-*.zip

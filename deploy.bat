2d3225bbf1801c29c4643dc76e18d83d74639283
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-09-04 10:40:00 -0400

    Added AssemblyFileVersion Updated version file for 1.12

--- deploy.bat
+++ deploy.bat
@@ -8,13 +8,17 @@ rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
 set H=%KSPDIR%
-set H=R:\KSP_1.10.1_CivPopJadeOfMaar
 set GAMEDIR=CivilianPopulation
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
+
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
-
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
